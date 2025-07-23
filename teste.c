ae7c0ebda3db6726d989ce4a2d9204e5a8eeb4c3
SaymonHR <saymonhr123@gmail.com>
2025-07-23 18:25:20 +0000

    ajustando

--- teste.c
+++ teste.c
@@ -6,53 +6,58 @@ int main()
     printf("Novo commit\n");
 
     // atributos da primeira carta:
-    char nome_da_cidade;
-    int populacao;
-    float area;
-    float pib;
-    int numero_pontos_turisticos;
-
-    // atributos da segunda carta:
-    char nome_da_cidade;
-    int populacao;
-    float area;
-    float pib;
-    int numero_pontos_turisticos;
+    char estado1 = 'A';
+    char codigo_da_carta1[5] = "a01";
+    char nome_da_cidade1[20] = "belem";
+    int populacao1;
+    float area1;
+    float pib1;
+    int numero_de_pontos_turisticos1;
 
     // Entrada de dados da primeira carta
-    printf("Qual o nome da sua cidade:\n");
-    scanf(" %s", &nome_da_cidade);
+
+    printf("Qual o seu Estado:\n");
+    scanf("%s", &estado1);
+
+    printf("Qual o codigo da carta:\n");
+    scanf("%s", &codigo_da_carta1);
+
+    printf("Qual o nome da cidade:");
+    scanf("%s", &nome_da_cidade1);
 
     printf("Qual a populacao da sua cidade:\n");
-    scanf(" %d", &populacao);
+    scanf(" %d", &populacao1);
 
     printf("qual o tamanho da area da sua cidade:\n");
-    scanf(" %f", &area);
+    scanf(" %f", &area1);
 
     printf("qual o pib da sua cidade:\n");
-    scanf(" %s", &pib);
+    scanf(" %f", &pib1);
 
     printf("qual o numero de pontos turisticos da sua cidade:\n");
-    scanf(" %d", &numero_pontos_turisticos);
+    scanf(" %d", &numero_de_pontos_turisticos1);
 
-
-
+    // atributos da segunda carta:
+    char estado2 = 'B';
+    char codigo_da_carta2[5] = "bo2";
+    char nome_da_cidade2[20] = "manaus";
+    int populacao2 = 1000;
+    float area2;
+    float pib2;
+    int numero_de_pontos_turisticos2;
 
     // entrada de dados da segunda carta:
-    printf("Qual o nome da sua cidade:\n");
-    scanf(" %s", &nome_da_cidade);
-
     printf("Qual a populacao da sua cidade:\n");
-    scanf(" %d", &populacao);
+    scanf(" %d", &populacao2);
 
     printf("qual o tamanho da area da sua cidade:\n");
-    scanf(" %f", &area);
+    scanf(" %f", &area2);
 
     printf("qual o pib da sua cidade:\n");
-    scanf(" %s", &pib);
+    scanf(" %f", &pib2);
 
     printf("qual o numero de pontos turisticos da sua cidade:\n");
-    scanf(" %d", &numero_pontos_turisticos);
+    scanf(" %d",&numero_de_pontos_turisticos2);
 
     return 0;
 }
