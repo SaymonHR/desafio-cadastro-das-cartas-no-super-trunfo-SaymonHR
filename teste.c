709e3a449f6e811eb32f12a89821db6f042fee33
SaymonHR <saymonhr123@gmail.com>
2025-07-23 16:03:15 +0000

    Dividi os atributos de cada carta

--- teste.c
+++ teste.c
@@ -1,16 +1,58 @@
 #include <stdio.h>
 
-int main();
+int main()
 {
     printf("Desafio Super Trunfo - Países\n");
     printf("Novo commit\n");
 
+    // atributos da primeira carta:
+    char nome_da_cidade;
     int populacao;
     float area;
     float pib;
     int numero_pontos_turisticos;
 
-    printf("Qual a populacao sua cidade:"\n);
-    scanf("%d", &populacao);
+    // atributos da segunda carta:
+    char nome_da_cidade;
+    int populacao;
+    float area;
+    float pib;
+    int numero_pontos_turisticos;
+
+    // Entrada de dados da primeira carta
+    printf("Qual o nome da sua cidade:\n");
+    scanf(" %s", &nome_da_cidade);
+
+    printf("Qual a populacao da sua cidade:\n");
+    scanf(" %d", &populacao);
+
+    printf("qual o tamanho da area da sua cidade:\n");
+    scanf(" %f", &area);
+
+    printf("qual o pib da sua cidade:\n");
+    scanf(" %s", &pib);
+
+    printf("qual o numero de pontos turisticos da sua cidade:\n");
+    scanf(" %d", &numero_pontos_turisticos);
+
+
+
+
+    // entrada de dados da segunda carta:
+    printf("Qual o nome da sua cidade:\n");
+    scanf(" %s", &nome_da_cidade);
+
+    printf("Qual a populacao da sua cidade:\n");
+    scanf(" %d", &populacao);
+
+    printf("qual o tamanho da area da sua cidade:\n");
+    scanf(" %f", &area);
+
+    printf("qual o pib da sua cidade:\n");
+    scanf(" %s", &pib);
+
+    printf("qual o numero de pontos turisticos da sua cidade:\n");
+    scanf(" %d", &numero_pontos_turisticos);
+
     return 0;
 }
