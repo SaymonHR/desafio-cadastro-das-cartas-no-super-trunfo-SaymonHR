b44aa6ca076c9ca5c1b9a89e33259aa1b3434f15
SaymonHR <saymonhr123@gmail.com>
2025-07-23 14:57:42 +0000

    criei um novo printf

--- teste.c
+++ teste.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
-int main(){
+int main();
+{
     printf("Desafio Super Trunfo - Países\n");
     printf("Novo commit\n");
 
+    int populacao;
+    float area;
+    float pib;
+    int numero_pontos_turisticos;
+
+    printf("Qual a populacao sua cidade:"\n);
+    scanf("%d", &populacao);
+    return 0;
 }
